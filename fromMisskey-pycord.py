b43f7944e495798ea993dfa417cfa4b92647cfe2
daizu <daizu007public@gmail.com>
2023-07-16 01:42:27 +0900

    change to use pycord

--- fromMisskey-pycord.py
+++ fromMisskey-pycord.py
@@ -6,18 +6,22 @@ import requests
 import discord
 import os
 
+#初期設定
+#デバックように区切りを定義
+line = "========================================================================================================================================================================"
 #botの権限 .allで全ての権限を与える。
-intents = discord.Intents().all() 
+intents = discord.Intents.default()
 #botを定義する
 bot = discord.Client(intents=intents)
 #discordのトークン
 dcToken = os.environ["dctoken"]
 #misskeyサーバーのURLの設定
-misskey_url = "wss://mk.shrimpia.network/streaming" #shrinpiaのアドレス
+misskey_url = "wss://misskey.io/streaming" #misskey.ioのアドレス
+#discord web hookのURLを設定
+discord_url = os.environ["DcURL"]
 
-print(dcToken)
 #misskey側での処理
-async def misskey():
+async def GetFromMisskey():
     try:
         #設定したmisskeyのURLでmisskeyにストリーミング接続
         async with websockets.connect(misskey_url) as ws:
@@ -34,37 +38,51 @@ async def misskey():
                 data = json.loads(await ws.recv())
                 #print(data)
                 if data["type"] == "channel":
-                    if data["body"]["type"] == "note":
-                        note = data['body']['body']
-                        await discord(note)
+                    await PostToDiscord(data)#PostToDiscordを呼び出す
 
     #エラーが起きたら内容を表示する
     except Exception as e:
-        print("errer at misskey function. errer is "+ e)
+        print("error at GetFromMisskey function. error is "+ str(e))
 
 #discord側での処理
-async def discord(note):
+async def PostToDiscord(data):
     try:
-        #データを取得
-        user = note["user"]#ユーザー
-        name_of_user = user["name"]#ユーザー名
-        avatar_of_user = user["avatarUrl"]#プロフィール画像のURL
-        content = note["text"]#ノートのテキスト
-        #TLに表示する内容
-        main_content = {
-            "username": name_of_user,
-            "avatar_url": avatar_of_user,
-            "content": content}
+        if data["body"]["type"] == "note":#もし新規ノートなら
+            #データを取得
+            note = data['body']['body']
+            user = note["user"]#ユーザー
+            name_of_user = user["name"]#ユーザー名
+            avatar_of_user = user["avatarUrl"]#プロフィール画像のURL
+            note_text = note["text"]#ノートのテキスト
+            note_url = f"https://misskey.io/notes/{data['body']['body']['id']}"
 
-        #headerを設定してTLに送信
-        headers = {'Content-Type': 'application/json'}
-        requests.post(discord_url, json.dumps(main_content), headers=headers)
+            #ユーザー名が取得できないユーザーへの特例処置
+            if name_of_user == None:
+                name_of_user = "名無しのMisskey.io民"
+            
+            #TLに表示する内容を作成（埋め込み）
+            embed = discord.Embed(title=name_of_user, description=note_text, url=note_url)
+            embed.set_author(name=name_of_user, icon_url=avatar_of_user, url=f"https://misskey.io/users/{data['body']['body']['user']['id']}")
+
+            #TLに表示する内容と受信した内容を表示（デバック用）
+            print(f"{line}\n title: {name_of_user}\n url: {note_url}\n text: {note_text}\n{line}\n"+json.dumps(note, indent=4))
+
+            #TLに送信
+            await timeline.send(embed=embed)
     
     #エラーが起きたら内容を表示する
     except Exception as e:
-        print("errer at discord function. errer is "+ e)
+        print("error at PostToDiscord function. error is "+ str(e))
+
+    
+#botの起動時に実行
+@bot.event
+async def on_ready():
+    global timeline
+    timeline = discord.utils.get(bot.get_all_channels(), name='👥timeline')
+    await GetFromMisskey()
 
 #実行
-#asyncio.run(misskey())
-#asyncio.run(discord())
+#asyncio.run(GetFromMisskey())
+#asyncio.run(PostToDiscord())
 bot.run(dcToken)
